e7c7852fd8510306f0487f96317b14a446574ee0
Vincent Damewood <git@vdamewood.com>
2015-12-29 10:18:00 -0800

    Change setup for protected mode

--- stage2/pmode.asm
+++ stage2/pmode.asm
@@ -1,9 +1,14 @@
 [ORG 0x10800]
-
+[BITS 32]
 PMode:
-	mov eax, 0xFEEDFACE
-	mov ecx, 0xF00DD00D
-	mov edx, 0xBAADBEEF
+	mov eax, 0x10
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	mov esp, 0x90000
+	mov ebp, esp
 .jmpy:
 	hlt
 	jmp .jmpy
