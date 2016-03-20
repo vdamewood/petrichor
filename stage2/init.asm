22c2bfc8f9f53c1e441cd6c7718fbd9c147116bb
Vincent Damewood <git@vdamewood.com>
2016-03-20 11:18:23 -0700

    Add code to clear .bss section on startup

--- stage2/init.asm
+++ stage2/init.asm
@@ -30,10 +30,13 @@ extern CommandLoop
 extern IntrSetupInterrupts
 extern ScreenClear
 extern ScreenPrintLine
+extern Compare
 
 SECTION .data
 
 Loaded: db 'Second stage loaded.', 0
+BssSignature: db '.bss', 0
+BssNotFound: db 'Warning: Unable to clear .bss.', 0
 
 SECTION .text
 
@@ -47,6 +50,61 @@ Init:
 	mov ss, ax
 	mov esp, 0x90000
 	mov ebp, esp
+LoadBss:
+	mov esi, [0x10020] ; Section Table Offset
+	add esi, 0x10000 ; Convert offset to memory
+
+	; Entry = SectionTable + EntrySize * StringIndex
+	xor eax, eax
+	xor edx, edx
+	mov ax, word[0x1002E] ; Size of Entry in section table
+	mov dx, word[0x10032] ; Index of .shstrtab section, in section table
+	mul dx
+	shl edx, 16
+	or eax, edx
+	add eax, esi ; We now have the offset within the table, add offset to base
+
+	mov ebx, eax
+	add ebx, 0x10      ; Byte 0x10 of a table entry the section's offset
+	mov ebx, [ebx]
+	add ebx, 0x10000   ; ebx now has the memory address of .shstrtab
+
+	xor ecx, ecx
+	xor edx, edx
+	mov dx, word[0x1002E] ; Load Entry size... again
+	mov cx, word[0x10030] ; Count
+
+	push 4
+	push BssSignature
+.BssScanLoop:
+	mov eax, [esi]
+	add eax, ebx
+	push eax
+
+	call Compare
+	pop eax
+	jz .BssFound
+
+	add esi, edx
+	loop .BssScanLoop
+.BssNotFound:
+	push BssNotFound
+	call ScreenPrintLine
+	add esp, 12
+	jmp .BssDone
+.BssFound:
+	add esp, 8
+
+	mov eax, [esi]
+	add eax, ebx
+
+	xor eax, eax
+	mov edi, [esi+0x0C] ; Address of .bss
+	mov ecx, [esi+0x14] ; Size of .bss
+	rep stosb
+.BssDone:
+
+
 	call IntrSetupInterrupts
 .loop:
 	call ScreenClear
